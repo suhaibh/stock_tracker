02aa8933aec1675ab9df594313bbefadc0f46c50
suhaibh <suhaib.habibullah@gmail.com>
2016-08-21 21:47:28 -0400

    Add spinner for stocks

--- app/assets/javascripts/stock.js
+++ app/assets/javascripts/stock.js
@@ -3,6 +3,14 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 var init_stock_lookup = function(){
+	$('#stock-lookup-form').on('ajax:before', function(event, data, status){
+		showSpinner();
+	});
+	
+	$('#stock-lookup-form').on('ajax:after', function(event, data, status){
+		hideSpinner();
+	});
+
 	$('#stock-lookup-form').on('ajax:success', function(event, data, status){
 		// This will add the searched stock's information to the page
 		$('#stock-lookup').replaceWith(data);
@@ -12,6 +20,7 @@ var init_stock_lookup = function(){
 	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
 		$('#stock-lookup-results').replaceWith(' ');
 		$('#stock-lookup-errors').replaceWith('Stock was not found.');
+		hideSpinner();
 	});
 }
 
