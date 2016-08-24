89cdb14ce5a284867bf410d45a42e1bbdd12bdfe
suhaibh <suhaib.habibullah@gmail.com>
2016-08-23 23:06:45 -0400

    List stocks users are following and allow for delete

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
   def my_portfolio
+  	@user = current_user
+  	@user_stocks = current_user.user_stocks
   end
 end
--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,5 @@
 class UserStocksController < ApplicationController
-  before_action :set_user_stock, only: [:show, :edit, :update, :destroy]
+  before_action :set_user_stock, only: [:destroy]
 
   # GET /user_stocks
   # GET /user_stocks.json
@@ -71,7 +71,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_url, notice: 'Stock was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
